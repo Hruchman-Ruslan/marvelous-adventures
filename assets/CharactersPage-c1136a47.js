8650fb787cd3b265bde53f2ad0203dcb18f2e0ee
Hruchman-Ruslan <Hruchman-Ruslan@users.noreply.github.com>
2023-09-26 19:46:17 +0000

    deploy: 228b809d34c78625401d32a805717de45a2d9fd5

---
+++ assets/CharactersPage-c1136a47.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-3ed4d37a.js";import{L as t}from"./Logo-018d5acb.js";const e=()=>r.jsx(r.Fragment,{children:r.jsx(t,{})});function n(){return r.jsx(r.Fragment,{children:r.jsx(e,{})})}export{n as default};
